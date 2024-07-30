a0b029487974cb41603d342dd018c51a105fa85e
root <markuj5@gmail.com>
2024-07-30 12:26:54 +0200

    Add LXD provisioning content

---
+++ bootstrap-kube.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# This script has been tested on Ubuntu 22.04
+# For other versions of Ubuntu, you might need some tweaking
+
+echo "[TASK 0] Install and initialize LXD"
+snap install lxd
+lxd init --auto
+sysctl -w net/netfilter/nf_conntrack_max=131072
+
+echo "[TASK 1] Install essential packages"
+export DEBIAN_FRONTEND=noninteractive
+apt-get update -qq >/dev/null
+apt-get install -qq -y net-tools curl ssh software-properties-common >/dev/null
+
+echo "[TASK 2] Install containerd runtime"
+apt-get install -qq -y apt-transport-https ca-certificates curl gnupg lsb-release >/dev/null
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+apt-get update -qq >/dev/null
+apt-get install -qq -y containerd.io >/dev/null
+containerd config default > /etc/containerd/config.toml
+sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+systemctl restart containerd
+systemctl enable containerd >/dev/null
+
+echo "[TASK 3] Set up kubernetes repo"
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' > /etc/apt/sources.list.d/kubernetes.list
+
+echo "[TASK 4] Install Kubernetes components (kubeadm, kubelet and kubectl)"
+apt-get update -qq >/dev/null
+apt-get install -qq -y kubeadm kubelet kubectl >/dev/null
+echo 'KUBELET_EXTRA_ARGS="--fail-swap-on=false"' > /etc/default/kubelet
+systemctl restart kubelet
+
+echo "[TASK 5] Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config
+sed -i 's/^KbdInteractiveAuthentication .*/KbdInteractiveAuthentication yes/' /etc/ssh/sshd_config
+systemctl reload sshd
+
+echo "[TASK 6] Set root password"
+echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
+echo "export TERM=xterm" >> /etc/bash.bashrc
+
+#######################################
+# To be executed only on master nodes #
+#######################################
+
+if [[ $(hostname) =~ .*master.* ]]
+then
+
+  echo "[TASK 7] Pull required containers"
+  kubeadm config images pull >/dev/null 2>&1
+
+  echo "[TASK 8] Initialize Kubernetes Cluster"
+  kubeadm init --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=all >> /root/kubeinit.log 2>&1
+
+  echo "[TASK 9] Copy kube admin config to root user .kube directory"
+  mkdir /root/.kube
+  cp /etc/kubernetes/admin.conf /root/.kube/config
+
+  echo "[TASK 10] Deploy Calico network"
+  kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/tigera-operator.yaml >/dev/null
+  kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/custom-resources.yaml >/dev/null
+
+  echo "[TASK 11] Generate and save cluster join command to /joincluster.sh"
+  joinCommand=$(kubeadm token create --print-join-command 2>/dev/null)
+  echo "$joinCommand --ignore-preflight-errors=all" > /joincluster.sh
+
+fi
+
+#######################################
+# To be executed only on worker nodes #
+#######################################
+
+if [[ $(hostname) =~ .*worker.* ]]
+then
+  echo "[TASK 7] Join node to Kubernetes Cluster"
+  apt install -qq -y sshpass >/dev/null 2>&1
+  sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no master.lxd:/joincluster.sh /joincluster.sh 2>/tmp/joincluster.log
+  bash /joincluster.sh >> /tmp/joincluster.log 2>&1
+fi
